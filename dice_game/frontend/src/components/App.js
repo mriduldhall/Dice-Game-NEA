4050fecf3cfdb0d6794832c337332a2b72ed6b3a
mriduldhall <mriduldhall1@gmail.com>
2021-09-13 20:25:29 +0100

    Built basic structure for React and Django

---
+++ dice_game/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+
+export default function App(props) {
+    return (
+        <h1>---In Development---</h1>
+    );
+}
+
+const appDiv = document.getElementById("app");
+render(<App/>, appDiv);
