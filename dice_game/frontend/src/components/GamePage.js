36a4c316aa7d983708fa870d1a91d169f29f8a2c
mriduldhall <mriduldhall1@gmail.com>
2021-09-30 19:03:53 +0100

    Added pause button and basic paused menu

--- dice_game/frontend/src/components/GamePage.js
+++ dice_game/frontend/src/components/GamePage.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import PauseCircleFilled from "@material-ui/icons/PauseCircleFilled";
+import PlayCircleFilled from "@material-ui/icons/PlayCircleFilled";
+import {Grid, Typography, Card, withStyles} from "@material-ui/core";
+
+
+const CherryRedTextTypography = withStyles({
+  root: {
+    color: "#f31a4c",
+  }
+})(Typography);
 
 
 export default function GamePage(props) {
+    let [pauseStatus, setPauseStatus] = useState(false)
+
+    function renderPauseMenu() {
+        return (
+            <Grid style={{background: "transparent"}}>
+                <PlayCircleFilled
+                    className={"top-right"}
+                    fontSize={"large"}
+                    style={{fill: "white"}}
+                    onClick={() => {
+                        setPauseStatus(false)
+                    }}
+                />
+                <Card className={"center"} style={{backgroundColor: "#442424", padding:10}}>
+                    <CherryRedTextTypography variant={"h3"} compact={"h3"}>
+                        Paused
+                    </CherryRedTextTypography>
+                </Card>
+            </Grid>
+        );
+    }
+
+    function renderPauseButton() {
+        return (
+            <PauseCircleFilled
+                className={"top-right"}
+                fontSize={"large"}
+                style={{fill: "white"}}
+                onClick={() => {
+                    setPauseStatus(true)
+                }}
+            />
+        );
+    }
+
     function renderGamePage() {
         return (
             <div className={"center game-background"}>
-
+                {
+                    (pauseStatus === false) ?
+                        renderPauseButton():
+                        renderPauseMenu()
+                }
             </div>
         );
     }
